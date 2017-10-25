e46eab780a4d48ff724bf9bfb42c7958c37d04fc
royalmar <royal-mar@hotmail.com>
2017-10-25 13:45:01 +0800

    [feature] 寄mail用queue的方式

--- app/Services/MailService.php
+++ app/Services/MailService.php
@@ -23,7 +23,7 @@ class MailService
 
         $data['link'] = 'http://172.104.83.229/validateEmail/' . $member->validEmailCode;
 
-        $this->send('歡迎使用 CityPass 城市通 - 註冊成功認證', $recipient, 'emails/register', $data);
+        return $this->send('歡迎使用 CityPass 城市通 - 註冊成功認證', $recipient, 'emails/register', $data);
     }
 
     /**
@@ -40,7 +40,7 @@ class MailService
 
         $data['link'] = 'http://172.104.83.229/validateEmail/' . $member->validEmailCode;
 
-        $this->send('CityPass 城市通 - Email認證信', $recipient, 'emails/validateEmail', $data);
+        return $this->send('CityPass 城市通 - Email認證信', $recipient, 'emails/validateEmail', $data);
     }
 
     /**
@@ -61,7 +61,7 @@ class MailService
 
         $data['name'] = $member->name;
 
-        $this->send('CityPass 城市通 - 密碼重設連結', $recipient, 'emails/forgetPassword', $data);
+        return $this->send('CityPass 城市通 - 密碼重設連結', $recipient, 'emails/forgetPassword', $data);
     }
 
     /**
@@ -90,36 +90,11 @@ class MailService
                 $message->from($from['email'], $from['name']);
                 $message->to($to['email'], $to['name'])->subject($from['subject']);
             });
+
+            return true;
         } catch(\Exception $e) {
             Log::error('smtp 有問題, 請檢查!');
+            return false;
         }
-    }
-
-    /**
-     * 非同步寄信 (未實作queue)
-     *
-     * @param string $subject
-     * @param array $recipient
-     * @param string $view
-     * @param array $viewData
-     * @return \App\Models\Member
-     */
-    public function queue($subject, $recipient, $view, $viewData = [])
-    {
-        $from = [
-            'email' => env('MAIL_USERNAME', 'ksd0045ksd@gmail.com'),
-            'name' => 'CityPass 城市通',
-            'subject' => ($subject) ?: 'CityPass 城市通通知信'
-        ];
-
-        $to = [
-            'email' => $recipient['email'],
-            'name' => (isset($recipient['name'])) ? $recipient['name'] : ''
-        ];
-
-        Mail::queue($view, $viewData, function ($message) use ($from, $to) {
-            $message->from($from['email'], $from['name']);
-            $message->to($to['email'], $to['name'])->subject($from['subject']);
-        });
     }
 }
