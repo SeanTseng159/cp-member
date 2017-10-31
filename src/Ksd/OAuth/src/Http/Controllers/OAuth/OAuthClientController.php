185cff81b1ab6cb011082ec866ac9507ff86fe6c
royalmar <royal-mar@hotmail.com>
2017-10-31 10:38:23 +0800

    [fix] 移除驗證正確網址

--- src/Ksd/OAuth/src/Http/Controllers/OAuth/OAuthClientController.php
+++ src/Ksd/OAuth/src/Http/Controllers/OAuth/OAuthClientController.php
@@ -85,7 +85,7 @@ class OAuthClientController extends BaseController
             'response_type' => 'required',
             'client_id' => 'required',
             'code' => 'required',
-            'redirect_url' => 'required|active_url'
+            'redirect_url' => (env('APP_ENV') === 'production') ? 'required|active_url' : 'required'
         ]);
 
         $request->session()->put('redirect_url', $data['redirect_url']);
