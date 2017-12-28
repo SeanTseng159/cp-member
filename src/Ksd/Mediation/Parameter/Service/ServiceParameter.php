42b0b529b81d2419c33bbc1835471ba6ccce15a0
vc <vc@gmail.com>
2017-12-28 15:54:43 +0800

    [feature]  新增客服中心API

---
+++ src/Ksd/Mediation/Parameter/Service/ServiceParameter.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Jim
+ * Date: 2017/9/15
+ * Time: 上午 09:37
+ */
+
+namespace Ksd\Mediation\Parameter\Service;
+
+use Ksd\Mediation\Config\ProjectConfig;
+use Ksd\Mediation\Parameter\BaseParameter;
+
+class ServiceParameter extends BaseParameter
+{
+    private $magento;
+    private $tpass;
+
+    /**
+     * 處理 ci request
+     * @param $input
+     */
+    public function codeigniterRequest($input, $parameters = null)
+    {
+        $this->request($parameters);
+        parent::codeigniterRequest($input);
+    }
+
+    /**
+     * 處理 laravel request
+     * @param $request
+     */
+    public function laravelRequest($request)
+    {
+        parent::laravelRequest($request);
+        $this->wishlistItemId = $request->input('wishlistItemId');
+        $this->source = $request->input('source');
+    }
+
+
+}
---
+++ src/Ksd/Mediation/Services/ServiceService.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jofo
+ * Date: 2017/9/5
+ * Time: 上午 9:04
+ */
+
+namespace Ksd\Mediation\Services;
+
+
+use Ksd\Mediation\Repositories\ServiceRepository;
+
+
+class ServiceService
+{
+    private $repository;
+
+    public function __construct(MemberTokenService $memberTokenService)
+    {
+        $this->repository = new ServiceRepository($memberTokenService);
+    }
+
+    /**
+     * 取得常用問題
+     * @param $id
+     * @return array
+     */
+
+    public function qa($id)
+    {
+        return $this->repository->qa($id);
+    }
+
+
+    /**
+     * 問題與建議
+     * @param $parameters
+     * @return bool
+     */
+    public function suggestion($parameters)
+    {
+        return $this->repository->suggestion($parameters);
+    }
+
+
+}
