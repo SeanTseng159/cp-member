2eb98790ec6cfd2746ca6e3c235079831b79d722
jofo <jofo.chang@touchcity.tw>
2018-01-09 17:04:01 +0800

    [revise] 調整台新付款回傳資料紀錄

--- src/Ksd/Mediation/Parameter/Checkout/ResultParameter.php
+++ src/Ksd/Mediation/Parameter/Checkout/ResultParameter.php
@@ -31,23 +31,23 @@ class ResultParameter extends BaseParameter
     public function laravelRequest($request = null)
     {
         parent::laravelRequest($request);
-        $this->ret_code = $request->input('ret_code');
-        $this->ret_msg = $request->input('ret_msg');
-        $this->order_no = $request->input('order_no');
-        $this->auth_id_resp = $request->input('auth_id_resp');
-        $this->rrn = $request->input('rrn');
-        $this->order_status = $request->input('order_status');
-        $this->auth_type = $request->input('auth_type');
-        $this->cur = $request->input('cur');
-        $this->purchase_date = $request->input('purchase_date');
-        $this->tx_amt = $request->input('tx_amt');
-        $this->settle_amt = $request->input('settle_amt');
-        $this->settle_seq = $request->input('settle_seq');
-        $this->settle_date = $request->input('settle_date');
-        $this->refund_trans_amt = $request->input('refund_trans_amt');
-        $this->refund_rrn = $request->input('refund_rrn');
-        $this->refund_auth_id_resp = $request->input('refund_auth_id_resp');
-        $this->refund_date = $request->input('refund_date');
+        $this->ret_code = $request->input('params.ret_code');
+        $this->ret_msg = $request->input('params.ret_msg');
+        $this->order_no = $request->input('params.order_no');
+        $this->auth_id_resp = $request->input('params.auth_id_resp');
+        $this->rrn = $request->input('params.rrn');
+        $this->order_status = $request->input('params.order_status');
+        $this->auth_type = $request->input('params.auth_type');
+        $this->cur = $request->input('params.cur');
+        $this->purchase_date = $request->input('params.purchase_date');
+        $this->tx_amt = $request->input('params.tx_amt');
+        $this->settle_amt = $request->input('params.settle_amt');
+        $this->settle_seq = $request->input('params.settle_seq');
+        $this->settle_date = $request->input('params.settle_date');
+        $this->refund_trans_amt = $request->input('params.refund_trans_amt');
+        $this->refund_rrn = $request->input('params.refund_rrn');
+        $this->refund_auth_id_resp = $request->input('params.refund_auth_id_resp');
+        $this->refund_date = $request->input('params.refund_date');
 
     }
 
