6fde3eb43ad252ee88e13586852206750448e589
jofo <jofo.chang@touchcity.tw>
2018-01-09 17:36:53 +0800

    [revise] 調整台新付款回傳接收資料方式

--- src/Ksd/Mediation/Parameter/Checkout/ResultParameter.php
+++ src/Ksd/Mediation/Parameter/Checkout/ResultParameter.php
@@ -31,24 +31,27 @@ class ResultParameter extends BaseParameter
     public function laravelRequest($request = null)
     {
         parent::laravelRequest($request);
-        $this->ret_code = $request->input('params.ret_code');
-        $this->ret_msg = $request->input('params.ret_msg');
-        $this->order_no = $request->input('params.order_no');
-        $this->auth_id_resp = $request->input('params.auth_id_resp');
-        $this->rrn = $request->input('params.rrn');
-        $this->order_status = $request->input('params.order_status');
-        $this->auth_type = $request->input('params.auth_type');
-        $this->cur = $request->input('params.cur');
-        $this->purchase_date = $request->input('params.purchase_date');
-        $this->tx_amt = $request->input('params.tx_amt');
-        $this->settle_amt = $request->input('params.settle_amt');
-        $this->settle_seq = $request->input('params.settle_seq');
-        $this->settle_date = $request->input('params.settle_date');
-        $this->refund_trans_amt = $request->input('params.refund_trans_amt');
-        $this->refund_rrn = $request->input('params.refund_rrn');
-        $this->refund_auth_id_resp = $request->input('params.refund_auth_id_resp');
-        $this->refund_date = $request->input('params.refund_date');
-
+        $body = $request->getContent();
+        if (!empty($body)) {
+            $parameters = json_decode($body, true);
+            $this->ret_code = array_get($parameters, 'params.ret_code');
+            $this->ret_msg = array_get($parameters, 'params.ret_msg');
+            $this->order_no = array_get($parameters, 'params.order_no');
+            $this->auth_id_resp = array_get($parameters, 'params.auth_id_resp');
+            $this->rrn = array_get($parameters, 'params.rrn');
+            $this->order_status = array_get($parameters, 'params.order_status');
+            $this->auth_type = array_get($parameters, 'params.auth_type');
+            $this->cur = array_get($parameters, 'params.cur');
+            $this->purchase_date = array_get($parameters, 'params.purchase_date');
+            $this->tx_amt = array_get($parameters, 'params.tx_amt');
+            $this->settle_amt = array_get($parameters, 'params.settle_amt');
+            $this->settle_seq = array_get($parameters, 'params.settle_seq');
+            $this->settle_date = array_get($parameters, 'params.settle_date');
+            $this->refund_trans_amt = array_get($parameters, 'params.refund_trans_amt');
+            $this->refund_rrn = array_get($parameters, 'params.refund_rrn');
+            $this->refund_auth_id_resp = array_get($parameters, 'params.refund_auth_id_resp');
+            $this->refund_date = array_get($parameters, 'params.refund_date');
+        }
     }
 
 }
