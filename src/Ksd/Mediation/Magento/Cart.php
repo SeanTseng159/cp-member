9ced7e4472f4e7d99db1c77447d492c5f615351c
vc <vc@gmail.com>
2018-01-16 13:49:29 +0800

    [revise] 取id修正

--- src/Ksd/Mediation/Magento/Cart.php
+++ src/Ksd/Mediation/Magento/Cart.php
@@ -117,16 +117,13 @@ class Cart extends Client
     public function add($parameters)
     {
 
-        $response = $this->request('GET', 'V1/carts/mine');
-        $result = json_decode($response->getBody(), true);
-        $cartId = $result['id'];
+        $cart = $this->detail();
 
         $data = ['quote' => [
             'items' => []
         ]];
-        if (!empty($cartId)) {
-            $data['quote']['id'] = $cartId;
-
+        if (!empty($cart->id)) {
+            $data['quote']['id'] = $cart->id;
         } else {
             $data['quote']['id'] = $this->createEmpty();
         }
