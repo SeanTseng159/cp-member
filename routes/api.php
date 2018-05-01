75539b1f6242243b3aafc385b7199c55f6140369
royalmar <royal-mar@hotmail.com>
2018-05-01 23:01:34 +0800

    [revise] 調整oauth查詢單一會員url

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,14 @@ use Illuminate\Http\Request;
 Route::get('test/regex', 'TestController@regex');
 // 需 管理者權限的token 認證的 route
 Route::middleware(['cors', 'admin.jwt'])->namespace('Api')->group(function () {
+    Route::prefix('oauth/member')->group(function () {
+        //單一會員資料查詢
+        Route::get('single/{id}', 'MemberController@singleMember');
+    });
+
     Route::prefix('member')->group(function () {
         //取所有會員
         Route::get('all', 'MemberController@allMember');
-        //單一會員資料查詢
-        Route::get('single/{id}', 'MemberController@singleMember');
         //會員資料查詢
         Route::get('query', 'MemberController@queryMember');
         //更新會員資料
