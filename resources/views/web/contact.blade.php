2f549c3eedee44b6594b5cc79ee86371cae1c36d
royalmar <royal-mar@hotmail.com>
2018-05-31 18:20:34 +0800

    [feature] 更改公司地址及電話

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -6,8 +6,7 @@
 <div class="terms-content">
     <div class="content text-center">
         <p>如您有任何問題，歡迎查看 <span class="btn-link color-orange">常見問題</span> 或致電我們的客服專線。</p>
-        <p>客服電話 (07)2265988 服務時間：週一至週五 9:00~18:​0​0
-（例假日及中午12:20~13:30暫不提供電話服務）</p>
+        <p>客服電話 (07) 752-8568 服務時間：週一至週五 9:00~18:​0​0 週六 10:00~18:​0​0（週日及中午12:20~13:30暫不提供電話服務）</p>
         <p>客服信箱 service@citypass.tw</p>
     </div>
 
