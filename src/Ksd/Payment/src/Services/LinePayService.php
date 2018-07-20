5a335a3f1a1707799824d6e604564b9c1aea4b41
royalmar <royal-mar@hotmail.com>
2018-07-20 16:24:52 +0800

    [revise] 調整linepay android導向

--- src/Ksd/Payment/src/Services/LinePayService.php
+++ src/Ksd/Payment/src/Services/LinePayService.php
@@ -48,8 +48,8 @@ class LinePayService
                     $cancelUrl = env('LINEPAY_ISO_REDIRECT') . 'failure/' . $order[0]->orderNo;
                 }
                 else if ($request->device === 'android') {
-                    $successUrl = 'intent://linepay/#Intent;scheme=citypass;package=tw.ksd.citypass;S.orderId=' . $order[0]->orderNo . ';B.success=true;end;';
-                    $cancelUrl = 'intent://linepay/#Intent;scheme=citypass;package=tw.ksd.citypass;S.orderId=' . $order[0]->orderNo . ';B.success=false;end;';
+                    $successUrl = env('LINEPAY_ANDROID_REDIRECT') . 'success/' . $order[0]->orderNo;
+                    $cancelUrl = env('LINEPAY_ANDROID_REDIRECT') . 'success/' . $order[0]->orderNo;
                 }
             }
 
