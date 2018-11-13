ae8edfd1889bead6588f838f2a5f1d7925944070
sheng <sheng.chen@touchcity.tw>
2018-11-13 16:40:58 +0800

    調整第3方登入api route

--- routes/api.php
+++ routes/api.php
@@ -273,6 +273,6 @@ Route::middleware('cors')->namespace('Api')->group(function () {
         Route::post('payNotify', 'IpassPayController@payNotify');
     });
     
-    Route::post('thirdPartyLogin', 'MemberController@thirdLogin');
+    Route::post('thirdPartyLogin', 'MemberController@thirdPartyLogin');
 
 });
