529a356173480dd7dbf6692a721a3d3ab9148298
royalmar <royal-mar@hotmail.com>
2018-12-06 17:35:34 +0800

    [fix] merge fix

--- app/Repositories/Ticket/OrderDetailRepository.php
+++ app/Repositories/Ticket/OrderDetailRepository.php
@@ -212,61 +212,6 @@ class OrderDetailRepository extends BaseRepository
     }
 
     /**
-     * 取票券列表
-     * @param $lang
-     * @param $parameter
-     * @return mixed
-     */
-    public function tickets($lang, $parameter)
-    {
-        if (!$parameter) return null;
-
-        $query = $this->model->with(['combo' => function($query) use ($parameter) {
-                                if ($parameter->orderStatus === '1' || $parameter->orderStatus === '2')
-                                return $query->orderBy('verified_at', 'desc');
-                            }])
-                            ->where('member_id', $parameter->memberId)
-                            ->where('ticket_show_status', 1)
-                            ->whereIn('prod_type', [1, 2, 3])
-                            ->where(function($query) use ($parameter) {
-                                if ($parameter->orderStatus === '4') {
-                                    return $query->where('order_detail_member_id', '!=', $parameter->memberId);
-                                }
-                                else {
-                                    return $query->where('order_detail_member_id', $parameter->memberId)->where('verified_status', $parameter->status);
-                                }
-                            })
-                            ->where(function($query) use ($parameter) {
-                                if ($parameter->orderStatus === '3') {
-                                    return $query->where('order_detail_expire_due', '<=', date('Y-m-d H:i:s'));
-                                }
-                                elseif ($parameter->orderStatus === '0') {
-                                    return $query->where('order_detail_expire_due', '>=', date('Y-m-d H:i:s'))
-                                        ->orWhere('order_detail_expire_due', null);
-                                }
-                            })
-                            ->offset($parameter->offset())
-                            ->limit($parameter->limit);
-
-        switch ($parameter->orderStatus) {
-            case '1':
-            case '2':
-                $orderDetails = $query->orderBy('verified_at', 'desc')->get();
-                break;
-            case '4':
-                $orderDetails = $query->orderBy('ticket_gift_at', 'desc')->get();
-                break;
-            default:
-                $orderDetails = $query->orderBy('created_at', 'desc')->get();
-                break;
-        }
-
-        if ($orderDetails->isEmpty()) return null;
-
-        return $orderDetails;
-    }
-
-    /**
      * 取會員某商品購買數
      * @param $productId
      * @param $memberId
