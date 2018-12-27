6463ec6ab520b4ce8b9edd7a11d0d06fd5a635ca
royalmar <royal-mar@hotmail.com>
2018-12-27 13:49:55 +0800

    [revise] 修改購物車優惠規則，調整收藏清單速度

--- app/Models/Ticket/ProductWishlist.php
+++ app/Models/Ticket/ProductWishlist.php
@@ -13,4 +13,18 @@ class ProductWishlist extends BaseModel
 {
     protected $table = 'prod_wishlists';
     protected $primaryKey = 'prod_wishlist_id';
+
+    /**
+     * 取得商品封面圖片
+     */
+    public function product()
+    {
+    	$now = date('Y-m-d H:i:s');
+
+        return $this->hasOne('App\Models\Ticket\Product', 'prod_id', 'prod_id')
+        			->notDeleted()
+        			->where('prod_onshelf', 1)
+        			->where('prod_onshelf_time', '<=', $now)
+                    ->where('prod_offshelf_time', '>=', $now);
+    }
 }
