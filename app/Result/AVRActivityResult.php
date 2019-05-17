c6791abe176b1f45889a4b2f1587205f3356103c
annie liang <annie.liang@touchcity.tw>
2019-05-17 15:15:54 +0800

    [fix] bug

--- app/Result/AVRActivityResult.php
+++ app/Result/AVRActivityResult.php
@@ -116,7 +116,7 @@ class AVRActivityResult extends BaseResult
         $ret->place = $mission->place_name;
         $ret->longitude = $mission->longitude;
         $ret->latitude = $mission->latitude;
-        $ret->checkGps = $mission->check_gps;
+        $ret->checkGps = (bool)$mission->check_gps;
         $ret->photo = AVRImageHelper::getImageUrl(AVRImageType::mission, $mission->id);
 
         //使用者相關
