c111db1f92f9ca518c173b725e819e565cecd5c5
annie liang <annie.liang@touchcity.tw>
2019-05-20 18:05:40 +0800

    [fix] bugs

--- app/Http/Controllers/Api/V1/AVRActivityController.php
+++ app/Http/Controllers/Api/V1/AVRActivityController.php
@@ -204,14 +204,15 @@ class AVRActivityController extends RestLaravelController
             if ($orderId == 0) $orderId = null;
 
             //檢查orderID是否屬於member
-            $orderRecord = $this->orderDetailService->find($orderId);
-            if (is_null($orderRecord)) {
-                throw  new \Exception('E0081');
+            if ($orderId) {
+                $orderRecord = $this->orderDetailService->find($orderId);
+                if (is_null($orderRecord)) {
+                    throw  new \Exception('E0081');
+                }
+                if ($orderRecord->member_id != $memberID) {
+                    throw  new \Exception('E0081');
+                }
             }
-            if ($orderRecord->member_id != $memberID) {
-                throw  new \Exception('E0081');
-            }
-
             $ret = $this->missionService->delete($missionId, $memberID, $orderId);
             return $this->success();
 
