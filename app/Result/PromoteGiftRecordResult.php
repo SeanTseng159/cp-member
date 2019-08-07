f17da619f074d943d07e4735bb7d8407b0772374
sheng <sheng.chen@touchcity.tw>
2019-08-07 09:06:05 +0800

    我的禮物避免已過期與已使用混合

--- app/Result/PromoteGiftRecordResult.php
+++ app/Result/PromoteGiftRecordResult.php
@@ -17,12 +17,12 @@ class PromoteGiftRecordResult
             $data->id = $item->id;
             $data->name = $item->promoteGift->name;
             $data->title = $item->promoteGift->name;
-            $data->duration = Carbon::parse($item->promoteGift->award_validity_end_at)->format('Y-m-d');
+            $data->duration = Carbon::parse($item->promoteGift->usage_end_at)->format('Y-m-d');
             $data->photo = $img->folder ? CommonHelper::getBackendHost($img->folder . $img->filename . '_s.' . $img->ext) : '';
             $data->type = MyGiftType::PROMOTE_GIFT;
 
             //$status 0:可使用  1:已使用 2:已過期 3:免核銷
-            if (is_null($item->verified_at)) {
+            if (is_null($item->verifier_at)) {
                 if (Carbon::now()->greaterThan($item->promoteGift->usage_end_at)) {
                     $data->status = 2;
                 } else {
@@ -52,7 +52,7 @@ class PromoteGiftRecordResult
         $result->id = $promoteGiftRecord->id;
         $result->name = $promoteGiftRecord->promoteGift->name;
         $result->title = $promoteGiftRecord->promoteGift->name;
-        $result->duration = Carbon::parse($promoteGiftRecord->promoteGift->award_validity_end_at)->format('Y-m-d');
+        $result->duration = Carbon::parse($promoteGiftRecord->promoteGift->usage_end_at)->format('Y-m-d');
         $result->photo = $img->folder ? CommonHelper::getBackendHost($img->folder . $img->filename . '_s.' . $img->ext) : '';
         $result->type = MyGiftType::PROMOTE_GIFT;
         $result->content = $promoteGiftRecord->promoteGift->content;
@@ -64,14 +64,14 @@ class PromoteGiftRecordResult
             $result->status = 0;
         }
 
-        //已使用
-        if ($promoteGiftRecord->verified_at) {
-            $result->status = 1;
-        }
-
         //已過期
         if (Carbon::now() >= Carbon::parse($promoteGiftRecord->promoteGift->usage_end_at)) {
             $result->status = 2;
+        }
+
+        //已使用
+        if ($promoteGiftRecord->verifier_at) {
+            $result->status = 1;
         }
         return $result;
     }
