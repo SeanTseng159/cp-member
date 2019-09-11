aa0e1f60bb5b944fab149fd04f43654a85a435d0
KSD-Sandy <peiyu.changchien@touchcity.tw>
2019-09-11 09:35:52 +0800

    更新文字

--- resources/views/web/contact.blade.php
+++ resources/views/web/contact.blade.php
@@ -5,8 +5,7 @@
 
 <div class="terms-content">
     <div class="content text-center">
-        <p>如您有任何問題，歡迎查看 <span class="btn-link color-orange">常見問題</span> 或致電我們的客服專線。</p>
-        <p>客服電話 (07) 752-8568 服務時間：週一至週五 9:00~18:​0​0 週六 10:00~18:​0​0（週日及中午12:20~13:30暫不提供電話服務）</p>
+        <p>如您有任何問題，歡迎查看 <span class="btn-link color-orange">常見問題</span> 或留下您的訊息，我們會在服務時間以Email跟您聯繫。</p>
         <p>客服信箱 service@citypass.tw</p>
     </div>
 
