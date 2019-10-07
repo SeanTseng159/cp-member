2062d43d70c2a90da4adf8b3f8d46a5c664fc1b7
annie <annie.liang@touchcity.tw>
2019-10-07 16:40:34 +0800

    [feature] 問卷api

--- app/Http/Controllers/Api/V1/ShopQuestionController.php
+++ app/Http/Controllers/Api/V1/ShopQuestionController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 
-use App\Http\Requests\Api\ShopQuestionRequest;
+use App\Core\Logger;
 use App\Result\ShopQuestionResult;
 use App\Services\ShopQuestionService;
 use Illuminate\Http\Request;
@@ -42,30 +42,54 @@ class ShopQuestionController extends RestLaravelController
 
             return $this->success($data);
         } catch (\Exception $e) {
-
+            Logger::error('ShopQuestionController::get', $e->getMessage());
             return $this->failure('E0001', $e->getMessage());
         }
     }
 
-    public function create(Request $request, $shopId)
+    public function create(Request $request, $shopId, $versionId)
     {
 
+
         try {
+
+
             $validator = \Validator::make($request->all(),
                 [
                     'date' => 'required|date_format:Y-m-d',
                     'list' => 'required|array|min:2',
                     'list.*.id' => 'required|integer',
-                    'list.*.answer' => 'required',
+                    'list.*.answer' => 'required|string',
                 ]
             );
             if ($validator->fails()) {
                 throw new \Exception($validator->messages());
             }
-            if (!$shopId)
-                throw new \Exception('參數不正確');
-        } catch (\Exception $e) {
+            $shop = $this->service->get($shopId);
 
+            if (!$shop->canQuestionnaire)
+                throw new \Exception('店鋪尚未開放問卷');
+
+            if (is_null($shop->currentQuestion))
+                throw new \Exception('店鋪尚未設定問卷資料或沒有進行中的問卷');
+
+            if (count(optional($shop->currentQuestion)->topicList) < 1)
+                throw new \Exception('店鋪尚未設定問卷題目');
+
+            $date = $request->input('date');
+            $list = $request->input('list');
+            $memberId = $request->memberId;
+
+            $ary = [];
+            foreach ($list as $item) {
+                $ary[] = (object)$item;
+            }
+            //確認answer
+            $answerAry = $this->service->checkAnswer($versionId, $ary);
+            $data = $this->service->store($memberId, $date, $answerAry);
+            return $this->success($data);
+        } catch (\Exception $e) {
+            Logger::error('ShopQuestionController::create', $e->getMessage());
             return $this->failure('E0001', $e->getMessage());
         }
     }
