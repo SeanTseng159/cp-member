a8c7bc455afe30ad3b7af431bdd9e0ba6df3216e
KSD-Sandy <peiyu.changchien@touchcity.tw>
2019-12-11 17:55:52 +0800

    line callback route fix

--- routes/line.php
+++ routes/line.php
@@ -2,5 +2,5 @@
 
 Route::namespace('Web\Line')->group(function () {
 		Route::get('login/{platform?}', 'MemberController@login');
-		Route::get('memberCallback/{platform?}', 'MemberController@callback')->name('line.memberCallback');
+		Route::get('memberCallback', 'MemberController@callback')->name('line.memberCallback');
 });
