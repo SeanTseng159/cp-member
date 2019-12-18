043177035732f737b1c3ae6d2730a4c2cf06a9d1
KSD-Sandy <peiyu.changchien@touchcity.tw>
2019-12-18 17:49:22 +0800

    Revert "Merge branch 'feature/category' into develop"
    This reverts commit 3fe5787f607f8b429e1c9ff1213534a185a4584e, reversing
    changes made to 6593b07e25c6e8e5d88b715170238a6c61c6023a.

--- app/Services/Ticket/LayoutService.php
+++ app/Services/Ticket/LayoutService.php
@@ -93,7 +93,6 @@ class LayoutService extends BaseService
     {
         $data['category'] = $this->tagRepository->oneWithUpperId($lang, $id);
         $data['customizes'] = ($data['category']) ? $this->categoryRepository->allById($lang, $id) : [];
-        $data['news'] = $this->tagEventRepository->getByTagId($id);
 
         return $data;
     }
