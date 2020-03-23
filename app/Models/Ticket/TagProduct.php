04de4b8e7f4b87cc23854ee144b84dbf029d1503
water <water.lee@touchcity.tw>
2020-03-23 15:45:34 +0800

    set tag & tag_product relation

--- app/Models/Ticket/TagProduct.php
+++ app/Models/Ticket/TagProduct.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * User: lee
  * Date: 2018/05/29
@@ -13,4 +14,12 @@ class TagProduct extends BaseModel
 {
     protected $table = 'tag_prods';
     protected $primaryKey = 'tag_prod_id';
+
+    /**
+     * 取得產品類別
+     */
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Ticket\Tag', 'tag_id', 'tag_id');
+    }
 }
