c49796322c39f42ed9177bf169187bd7816666e7
jerry <jerry.liu@touchcity.tw>
2020-05-05 08:54:53 +0800

    fix v3

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -111,7 +111,20 @@ class RouteServiceProvider extends ServiceProvider
              ->group(base_path('routes/Api/V2.php'));
     }
 
-
+    /**
+     * Define the "api" routes for the application.
+     *
+     * These routes are typically stateless.
+     *
+     * @return void
+     */
+    protected function mapApiV3Routes()
+    {
+        Route::prefix('api/v3')
+             ->middleware('api')
+             ->namespace('App\Http\Controllers\Api')
+             ->group(base_path('routes/Api/V3.php'));
+    }
 
 
     /**
