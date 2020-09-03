1b55214a34a31707e6eb0ce8a13a426bc0fb3fba
Eli <eli.hsu@touchcity.tw>
2020-09-03 15:43:09 +0800

    修改優惠券過期與使用邏輯

--- app/Repositories/Ticket/MemberDiscountRepository.php
+++ app/Repositories/Ticket/MemberDiscountRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * User: Annie
  * Date: 2019/02/13
@@ -17,6 +18,7 @@ use DB;
 use App\Core\Logger;
 
 use  App\Models\Ticket\MemberDiscount;
+use Log;
 
 class MemberDiscountRepository extends BaseRepository
 {
@@ -24,117 +26,131 @@ class MemberDiscountRepository extends BaseRepository
 
     public function __construct(MemberDiscount $model)
     {
-        $this->model=$model;
-        
+        $this->model = $model;
     }
 
     public function listCanUsed($memberID)
     {
-        return $this->model->with([ 'discountCode',
-                                    'discountCodeBlock',
-                                    'discountCodeTag',
-                                    'discountCodeTag.tagProdId',
-                                    'discountCodeMember'])
-                            ->whereHas('discountCode',function ($query){
-                                $query->where('discount_code_status', 1)
-                                        ->where('deleted_at',0)
-                                        ->where('discount_code_starttime','<=',Carbon::today())
-                                        ->where('discount_code_endtime','>',Carbon::today());
-                            })
-                            ->where('member_id',$memberID)
-                            ->where('status',1)
-                            ->where('used',0)
-                            ->get();
+        return $this->model->with([
+            'discountCode',
+            'discountCodeBlock',
+            'discountCodeTag',
+            'discountCodeTag.tagProdId',
+            'discountCodeMember'
+        ])
+            ->whereHas('discountCode', function ($query) {
+                $query->where('discount_code_status', 1)
+                    ->where('deleted_at', 0)
+                    ->where('discount_code_starttime', '<=', Carbon::today())
+                    ->where('discount_code_endtime', '>', Carbon::today());
+            })
+            ->where('member_id', $memberID)
+            ->where('status', 1)
+            ->where('used', 0)
+            ->get();
     }
 
 
     public function listProdDiscount($memberID)
     {
-        return $this->model ->where('member_id',$memberID)
-                            ->get();
+        return $this->model->where('member_id', $memberID)
+            ->get();
     }
 
 
-    public function createAndCheck($data){
-        $check=$this->model->where('discount_code_id',$data['discount_code_id'])->where('member_id',$data['member_id'])->first();
-        if(empty($check)){
+    public function createAndCheck($data)
+    {
+        $check = $this->model->where('discount_code_id', $data['discount_code_id'])->where('member_id', $data['member_id'])->first();
+        if (empty($check)) {
             $this->model->create($data);
             return true;
-        }else{
+        } else {
             return false;
         }
-        
     }
 
     //取得有效票
-    public function current($memberID){
-        return $this->model->with(['discountCode',
-                                   'discountCode.discountCodeTag.tag',
-                                   'discountCode.discountCodeMember'=>function ($query) use($memberID){
-                                                $query->where('member_id',$memberID);
-                            }])
-                            ->whereHas('discountCode',function ($query){
-                                $query->where('discount_code_status', 1)
-                                        ->where('deleted_at',0)
-                                        ->where('discount_code_starttime','<=',Carbon::today())
-                                        ->where('discount_code_endtime','>',Carbon::today())
-                                        ->whereColumn('discount_code_limit_count','>','discount_code_used_count');
-                            })
-                            ->where('member_id',$memberID)
-                            ->where('status',1)
-                            ->where('used',0)
-                            ->get();
+    public function current($memberID)
+    {
+        return $this->model->with([
+            'discountCode',
+            'discountCode.discountCodeTag.tag',
+            'discountCode.discountCodeMember' => function ($query) use ($memberID) {
+                $query->where('member_id', $memberID);
+            }
+        ])
+            ->whereHas('discountCode', function ($query) {
+                $query->where('discount_code_status', 1)
+                    ->where('deleted_at', 0)
+                    ->where('discount_code_starttime', '<=', Carbon::today())
+                    ->where('discount_code_endtime', '>', Carbon::today())
+                    ->whereColumn('discount_code_limit_count', '>', 'discount_code_used_count');
+            })
+            ->where('member_id', $memberID)
+            ->where('status', 1)
+            ->where('used', 0)
+            ->get();
     }
 
     //取得失效
-    public function disabled($memberID){
-        //使用達上限的 但是優惠倦還在期間的
-        $data1= $this->model->with(['discountCode','discountCode.discountCodeTag.tag',])
-                            ->whereHas('discountCode',function ($query){
-                                $query->where('discount_code_status', 1)
-                                        ->where('deleted_at',0)
-                                        ->where('discount_code_endtime','>=',Carbon::today()->subMonths(1));
-                                        
-                            })
-                            ->where('member_id',$memberID)
-                            ->where('used',1)
-                            ->where('status',1)
-                            ->get();
-        
-   
-        //失效一個月內 或者沒有票了
-        $data2=$this->model->with(['discountCode','discountCode.discountCodeTag.tag',])
-                            ->orWhereHas('discountCode',function ($query){
-                                $query->where('discount_code_status',1)
-                                        ->where('deleted_at',0)
-                                        ->where('discount_code_endtime','>=',Carbon::today()->subMonths(1))
-                                        ->whereColumn('discount_code_limit_count','=','discount_code_used_count')
-                                        ->where('discount_code_endtime','>=',Carbon::today()->subMonths(1));
-                            })
-                            ->orWhereHas('discountCode',function ($query){
-                                $query->where('discount_code_status',1)
-                                        ->where('deleted_at',0)
-                                        ->where('discount_code_endtime','>=',Carbon::today()->subMonths(1))
-                                        ->where('discount_code_endtime','<=',Carbon::today());
-                                        
-                            })
-                            ->where('member_id',$memberID)
-                            ->where('used',0)
-                            ->get();
-        
-        
-        return collect($data1)->merge($data2);
+    public function disabled($memberID)
+    {
+        // 票過期，使用數量達上限，票後台設定關閉
+        $data2 = $this->model->with(['discountCode', 'discountCode.discountCodeTag.tag',])
+            ->orWhereHas('discountCode', function ($query) {
+                // 軟刪除
+                $query->where('deleted_at', 1)
+                    ->where('discount_code_endtime', '>=', Carbon::today()->subMonths(1))
+                    ->where('discount_code_endtime', '<=', Carbon::today());
+            })
+            ->orWhereHas('discountCode', function ($query) {
+                // 關閉
+                $query->where('discount_code_status', 0)
+                    ->where('discount_code_endtime', '>=', Carbon::today()->subMonths(1))
+                    ->where('discount_code_endtime', '<=', Carbon::today());
+            })
+            ->orWhereHas('discountCode', function ($query) {
+                // 使用數量達上限
+                $query->where('discount_code_status', 1)
+                    ->where('deleted_at', 0)
+                    ->whereColumn('discount_code_limit_count', '<=', 'discount_code_used_count')
+                    ->where('discount_code_endtime', '>=', Carbon::today()->subMonths(1))
+                    ->where('discount_code_endtime', '<=', Carbon::today());
+            })
+            ->orWhereHas('discountCode', function ($query) {
+                // 票過期
+                $query->where('discount_code_status', 1)
+                    ->where('deleted_at', 0)
+                    ->where('discount_code_endtime', '>=', Carbon::today()->subMonths(1))
+                    ->where('discount_code_endtime', '<=', Carbon::today());
+            })
+            ->where('member_id', $memberID)
+            ->where('used', 0)
+            ->where('status', 1)
+            ->get();
 
+        return collect($data2);
     }
 
-    
-    public function used($memberID){
-        //撈取最近一個月使用
-        return $this->model->with(['discountCodeMemberByMember','discountCodeMemberByMember.discountCode.discountCodeTag.tag'])
-                            ->where('member_id',$memberID)
-                            ->get();
 
+    public function used($memberID)
+    {
+
+        return $this->model->with([
+            'discountCode',
+            'discountCode.discountCodeTag.tag',
+            'discountCode.discountCodeMember' => function ($query) use ($memberID) {
+                $query->where('member_id', $memberID);
+            }
+        ])
+            ->whereHas('discountCode', function ($query) {
+                $query->where('discount_code_status', 1)
+                    ->where('deleted_at', 0);
+            })
+            ->where('member_id', $memberID)
+            ->where('status', 1)
+            ->where('used', 1)
+            ->where('created_at', '>=', Carbon::today()->subMonths(12)) //  請求日往回推一年，歸戶日要大於期間
+            ->get();
     }
-    
-
 }
--- app/Result/Ticket/MemberDiscountResult.php
+++ app/Result/Ticket/MemberDiscountResult.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by Fish.
  * 2019/12/23 4:44 下午
@@ -13,235 +14,220 @@ use Carbon\Carbon;
 class MemberDiscountResult extends BaseResult
 {
 
-    public function listCanUsed($member_discounts,$cartItems,$memberID)
+    public function listCanUsed($member_discounts, $cartItems, $memberID)
     {
         $resultObj = new \stdClass;
         $resultObj->useful = [];
         $resultObj->useless = [];
         // dd(empty($member_discounts[0]->discountCode));
-        foreach($member_discounts as $item){
-            $addBo=True;
-            $message='';
-            
+        foreach ($member_discounts as $item) {
+            $addBo = True;
+            $message = '';
+
             //購物車裏面沒有商品
-            if(empty($cartItems[0]->items)){
-                $addBo=false;
-                $message='沒有商品';
+            if (empty($cartItems[0]->items)) {
+                $addBo = false;
+                $message = '沒有商品';
             }
-            
+
             //discountCode 判斷　有效數量　　　優惠卷時間
-            if($addBo){
+            if ($addBo) {
                 //如果沒有優惠倦就到下個loop
-                if(empty($item->discountCode)){
+                if (empty($item->discountCode)) {
                     continue;
-                }elseif( $item->discountCode->discount_code_limit_count <= $item->discountCode->discount_code_used_count){
-                    $addBo=false;
-                    $message='優惠卷使用完畢';
+                } elseif ($item->discountCode->discount_code_limit_count <= $item->discountCode->discount_code_used_count) {
+                    $addBo = false;
+                    $message = '優惠卷使用完畢';
                 }
-                
             }
-            
+
 
 
             //拿出  discountCodeMember  =0 代表無上線
-            if($addBo){
-                if($item->discountCode->discount_code_member_use_count!=0){
+            if ($addBo) {
+                if ($item->discountCode->discount_code_member_use_count != 0) {
                     //檢查式否達會員可使用上限 //有效數量
-                    
+
                     //會員可使用上限
-                    $filtered = collect($item->discountCodeMember)->filter(function ($item) use($memberID) {
-                        return $item->member_id== $memberID;
+                    $filtered = collect($item->discountCodeMember)->filter(function ($item) use ($memberID) {
+                        return $item->member_id == $memberID;
                     });
-    
-                    if(collect($filtered)->count() >= $item->discountCode->discount_code_member_use_count){
-                        $addBo=false;
-                        $message='超過可使用上限';
+
+                    if (collect($filtered)->count() >= $item->discountCode->discount_code_member_use_count) {
+                        $addBo = false;
+                        $message = '超過可使用上限';
                     }
-                      
                 }
 
                 //首購
-                if($item->discountCode->discount_first_type==1){
+                if ($item->discountCode->discount_first_type == 1) {
                     //檢查是否為首物購買
-                    if(collect($item->discountCodeMember)->count()>0){
-                        $addBo=false;
-                        $message='非首購';
-                    }  
+                    if (collect($item->discountCodeMember)->count() > 0) {
+                        $addBo = false;
+                        $message = '非首購';
+                    }
                 }
+            } //end discountCodeMember
 
-            }//end discountCodeMember
-            
             //取出 discount_code_block_prods  
             //discount_code_tags with tag_prods 檢查是否可以使用範圍的商品
-            if($addBo){
+            if ($addBo) {
                 //將優惠倦拒絕的prodId列出來 存在   $blockProdIdArray    
-                $blockProdIdArray=[];       
-                foreach($item->discountCodeBlock as $tmp){
-                    $blockProdIdArray[]=$tmp->prod_id;
+                $blockProdIdArray = [];
+                foreach ($item->discountCodeBlock as $tmp) {
+                    $blockProdIdArray[] = $tmp->prod_id;
                 }
 
                 //將優惠倦接受prodId列出來 存在   $allowProdIdArray   
-                $allowProdIdArray=[];
-                foreach($item->discountCodeTag as $discountTag){
-                    $tmpProdIdArray=collect(collect($discountTag)->get('tag_prod_id'))->pluck('prod_id')->all();
-                    $allowProdIdArray=collect([$allowProdIdArray,$tmpProdIdArray])->collapse()->all();
+                $allowProdIdArray = [];
+                foreach ($item->discountCodeTag as $discountTag) {
+                    $tmpProdIdArray = collect(collect($discountTag)->get('tag_prod_id'))->pluck('prod_id')->all();
+                    $allowProdIdArray = collect([$allowProdIdArray, $tmpProdIdArray])->collapse()->all();
                 }
 
-                
+
                 //拿出購物車的商品
-                foreach($cartItems[0]->items as $cartItem){
-                    
-                    $cartProdsTmp=new \stdClass;
+                foreach ($cartItems[0]->items as $cartItem) {
+
+                    $cartProdsTmp = new \stdClass;
                     //判斷商品是否再拒絕的裏面
-                    if (in_array($cartItem->id, $blockProdIdArray)){
-                        
-                    //流下沒有背拒絕且接受的商品
-                    }elseif(in_array($cartItem->id, $allowProdIdArray)){
-                        $cartProdsTmp->prodId=$cartItem->id;
-                        $cartProdsTmp->price=$cartItem->price;
-                        $cartProdsTmp->qty=$cartItem->qty;
-                        $cartProdsTmp->specId=$cartItem->additionals['specId'];
-                        $cartProdsTmp->priceId=$cartItem->additionals['priceId'];
-                        $cartProdsPass[]=$cartProdsTmp;
+                    if (in_array($cartItem->id, $blockProdIdArray)) {
+
+                        //流下沒有背拒絕且接受的商品
+                    } elseif (in_array($cartItem->id, $allowProdIdArray)) {
+                        $cartProdsTmp->prodId = $cartItem->id;
+                        $cartProdsTmp->price = $cartItem->price;
+                        $cartProdsTmp->qty = $cartItem->qty;
+                        $cartProdsTmp->specId = $cartItem->additionals['specId'];
+                        $cartProdsTmp->priceId = $cartItem->additionals['priceId'];
+                        $cartProdsPass[] = $cartProdsTmp;
                     }
-                    
                 }
-                
-            }//end  discount_code_block_prods discount_code_tags
+            } //end  discount_code_block_prods discount_code_tags
 
 
-            if($addBo & empty($cartProdsPass)){
-                $addBo=false;
-                $message='商品不符合條件';
+            if ($addBo & empty($cartProdsPass)) {
+                $addBo = false;
+                $message = '商品不符合條件';
             }
 
             //dicount_codes 是否符合discount條件
-            if($addBo ){
-                $totalPrice=0;
-                foreach($cartProdsPass as $cartTmp){
-                    $totalPrice+=(Integer)$cartTmp->qty*(Integer)$cartTmp->price;
+            if ($addBo) {
+                $totalPrice = 0;
+                foreach ($cartProdsPass as $cartTmp) {
+                    $totalPrice += (int)$cartTmp->qty * (int)$cartTmp->price;
                 }
-                
+
                 //滿足最低折扣 //最低限制消費
                 //計算折扣
-                if( $item->discountCode->discount_code_type ==1){
-                    $totalPriceAfterDiscount=(Integer)($totalPrice*(Float)$item->discountCode->discount_code_price/100);
-                //計算折價
-                }elseif($item->discountCode->discount_code_type ==2){
-                    $totalPriceAfterDiscount=$totalPrice-(Integer)$item->discountCode->discount_code_price;
+                if ($item->discountCode->discount_code_type == 1) {
+                    $totalPriceAfterDiscount = (int)($totalPrice * (float)$item->discountCode->discount_code_price / 100);
+                    //計算折價
+                } elseif ($item->discountCode->discount_code_type == 2) {
+                    $totalPriceAfterDiscount = $totalPrice - (int)$item->discountCode->discount_code_price;
                 }
-                
-                
-                if( $item->discountCode->discount_code_limit_price >$totalPriceAfterDiscount){
-                    $addBo=false;
-                    $message='需要滿足最低消費';
+
+
+                if ($item->discountCode->discount_code_limit_price > $totalPriceAfterDiscount) {
+                    $addBo = false;
+                    $message = '需要滿足最低消費';
                 }
-                
-                
-
-
-
-            }//end dicount_codes 
+            } //end dicount_codes 
 
             // dd($item->discountCode);
-            $result=$this->getListResult($item,'null');
-            $result->status=$addBo;
-            $result->message=$message;
+            $result = $this->getListResult($item, 'null');
+            $result->status = $addBo;
+            $result->message = $message;
             // $result->id=$item->discountCode->discount_code_id;
             // $result->name=$item->discountCode->discount_code_name;
             // $result->value=$item->discountCode->discount_code_value;
             // $result->endTime=Carbon::parse($item->discountCode->discount_code_endtime)->format('Y-m-d');
-            $addBo ? $resultObj->useful[]=$result : $resultObj->useless[]=$result;
-        }//end foreach
+            $addBo ? $resultObj->useful[] = $result : $resultObj->useless[] = $result;
+        } //end foreach
 
         return $resultObj;
-    }//end 
+    } //end 
 
 
-    public function list($datas,$func){
+    public function list($datas, $func)
+    {
 
-        $resultAraray=[];
-        
+        $resultAraray = [];
+
         // dd($datas);
         // dd(collect($datas[1]->discountCode->discountCodeMember)->count());
-        foreach($datas as $key=>$item){
-            
-            switch ($func){
+        foreach ($datas as $key => $item) {
+
+            switch ($func) {
                 case 'current':
-                    $count=collect($item->discountCode->discountCodeMember)->count();
-                    if($item->discountCode->discount_code_member_use_count!=0 && $item->discountCode->discount_code_member_use_count <= $count ){
+                    $count = collect($item->discountCode->discountCodeMember)->count();
+                    if ($item->discountCode->discount_code_member_use_count != 0 && $item->discountCode->discount_code_member_use_count <= $count) {
                         //超過使用上線
-                    }elseif($count>0 && $item->discountCode->discount_first_type==1){
+                    } elseif ($count > 0 && $item->discountCode->discount_first_type == 1) {
                         //只有給首次購買
-                    }else{
-                        $resultAraray[]=$this->getListResult($item,$func);
+                    } else {
+                        $resultAraray[] = $this->getListResult($item, $func);
                     }
                     break;
                 case 'disabled':
-                    $resultAraray[]=$this->getListResult($item,$func);
-                    
+                    $resultAraray[] = $this->getListResult($item, $func);
+
                     break;
                 case 'used':
-                    foreach($item->discountCodeMemberByMember as $tmp){
-                        
-                        $resultAraray[]=$this->getListResult($tmp,$func);
-                    }
+                    $resultAraray[] = $this->getListResult($item, $func);
+
                     break;
             }
-
-            
-            
-            
         }
         return $resultAraray;
     }
 
-    public function getListResult($data,$func){
-        $result=new \stdClass;
-        $result->id=$data->discountCode->discount_code_id;
-        $result->name=$data->discountCode->discount_code_name;
-        $result->value=$data->discountCode->discount_code_value;
-        $result->desc= $data->discountCode->discount_code_desc;
-        $result->status=$func;
-        $result->orderNo=$data->order_no;
-        $result->endTime=Carbon::parse($data->discountCode->discount_code_endtime)->format('Y-m-d');
+    public function getListResult($data, $func)
+    {
+        $result = new \stdClass;
+        $result->id = $data->discountCode->discount_code_id;
+        $result->name = $data->discountCode->discount_code_name;
+        $result->value = $data->discountCode->discount_code_value;
+        $result->desc = $data->discountCode->discount_code_desc;
+        $result->status = $func;
+        $result->orderNo = $data->order_no;
+        $result->endTime = Carbon::parse($data->discountCode->discount_code_endtime)->format('Y-m-d');
         //$result->range=Carbon::parse($data->discountCode->discount_code_starttime)->format('Y-m-d').'~'.Carbon::parse($data->discountCode->discount_code_endtime)->format('Y-m-d');
-        $result->imageUrl=$this->getImg($data->discountCode->image_path);
-        $tag='';
-        foreach($data->discountCode->discountCodeTag as $item){
-            $tag=$tag.$item->tag->tag_name.',';
+        $result->imageUrl = $this->getImg($data->discountCode->image_path);
+        $tag = '';
+        foreach ($data->discountCode->discountCodeTag as $item) {
+            $tag = $tag . $item->tag->tag_name . ',';
         }
-        $result->category=substr($tag,0,-1);
+        $result->category = substr($tag, 0, -1);
         return $result;
     }
 
-    public function listByProd($member_discounts,$discountCodes){
+    public function listByProd($member_discounts, $discountCodes)
+    {
 
-        foreach($discountCodes as $itemDiscount){
+        foreach ($discountCodes as $itemDiscount) {
             //是否有這張discount
-            if(collect($member_discounts)->contains('discount_code_id',$itemDiscount->discount_code_id)){
-                $ownStatus=true;
-            }else{
-                $ownStatus =false;
+            if (collect($member_discounts)->contains('discount_code_id', $itemDiscount->discount_code_id)) {
+                $ownStatus = true;
+            } else {
+                $ownStatus = false;
             }
-            $resultObj=new \stdClass;
-            $resultObj->id= $itemDiscount->discount_code_id;
-            $resultObj->name= $itemDiscount->discount_code_name;
-            $resultObj->value= $itemDiscount->discount_code_value;
-            $resultObj->desc= $itemDiscount->discount_code_desc;
-            $resultObj->range=Carbon::parse($itemDiscount->discount_code_starttime)->format('Y-m-d').'~'.Carbon::parse($itemDiscount->discount_code_endtime)->format('Y-m-d');
-            $resultObj->imageUrl= $this->getImg($itemDiscount->image_path);
-            $tag='';
-            foreach($itemDiscount->discountCodeTag as $item){
-                $tag=$tag.$item->tag->tag_name.',';
+            $resultObj = new \stdClass;
+            $resultObj->id = $itemDiscount->discount_code_id;
+            $resultObj->name = $itemDiscount->discount_code_name;
+            $resultObj->value = $itemDiscount->discount_code_value;
+            $resultObj->desc = $itemDiscount->discount_code_desc;
+            $resultObj->range = Carbon::parse($itemDiscount->discount_code_starttime)->format('Y-m-d') . '~' . Carbon::parse($itemDiscount->discount_code_endtime)->format('Y-m-d');
+            $resultObj->imageUrl = $this->getImg($itemDiscount->image_path);
+            $tag = '';
+            foreach ($itemDiscount->discountCodeTag as $item) {
+                $tag = $tag . $item->tag->tag_name . ',';
             }
-            $resultObj->category=substr($tag,0,-1);
-            $resultObj->ownStatus= $ownStatus;
-      
-            $result[]=$resultObj;
+            $resultObj->category = substr($tag, 0, -1);
+            $resultObj->ownStatus = $ownStatus;
 
-
-        }//end foreach
+            $result[] = $resultObj;
+        } //end foreach
         return $result;
     }
 
@@ -254,6 +240,4 @@ class MemberDiscountResult extends BaseResult
     {
         return isset($imgs) ? $this->backendHost . $imgs : '';
     }
-
-
 }
